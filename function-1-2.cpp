bf120b714b3867c6612f144ffa0f2aaccffc1887
aishu <a1849800@adelaide.edu.au>
2025-03-19 13:14:19 +1030

    added code

---
+++ function-1-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std; 
+
+double array_mean(int array[], int n){
+    if (n < 1) return 0.0; 
+
+    double avg = 0;
+    int sum = 0;
+
+    for (int i = 0; i < n; i++) {
+        sum += array[i];
+        avg += sum/2;
+    }
+    return avg;
+}
