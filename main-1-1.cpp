900df331bec8e79470f2e5370235fcd26d1c8159
aishu <a1849800@adelaide.edu.au>
2025-03-19 12:55:39 +1030

    added code

---
+++ main-1-1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int array_sum(int array[], int n); 
+
+int main() {
+    int array[] = {1,2,3,4,5};
+    sum += array[];
+
+    cout << "The sum of all elements in the array is: " << array_sum(sum)
+}
